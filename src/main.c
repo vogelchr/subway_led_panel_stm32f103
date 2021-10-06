c6e57649c4eac79874b78b20980062678a10b678
Christian Vogel <vogelchr@vogel.cx>
2021-10-06 19:33:38 +0200

    add gpl headers

--- src/main.c
+++ src/main.c
@@ -1,3 +1,23 @@
+/*
+ * This file is part of subway_led_panel_stm32f103, originally
+ * distributed at https://github.com/vogelchr/subway_led_panel_stm32f103.
+ *
+ *     Copyright (c) 2021 Christian Vogel <vogelchr@vogel.cx>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include "hw_matrix.h"
 #include "ledpanel_buffer.h"
 #include "usb_if.h"
--- src/mtwister.c
+++ src/mtwister.c
@@ -1,3 +1,22 @@
+/*
+ * This file is part of subway_led_panel_stm32f103, originally
+ * distributed at https://github.com/vogelchr/subway_led_panel_stm32f103.
+ *
+ *     Copyright (c) 2021 Christian Vogel <vogelchr@vogel.cx>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 /* An implementation of the MT19937 Algorithm for the Mersenne Twister
  * by Evan Sultanik.  Based upon the pseudocode in: M. Matsumoto and
  * T. Nishimura, "Mersenne Twister: A 623-dimensionally
