d7afe1c5d1a19d7715737af7558021ca206190e5
Christian Vogel <vogelchr@vogel.cx>
2021-10-06 19:27:33 +0200

    hw_matrix.h: add comments, hw_matrix_mbi5029_mode

--- include/hw_matrix.h
+++ include/hw_matrix.h
@@ -1,8 +1,10 @@
 #ifndef HW_MATRIX_H
 #define HW_MATRIX_H
 
-extern void hw_matrix_init(void);
-extern void hw_matrix_stop(void);
-extern void hw_matrix_start(void);
+extern void hw_matrix_init(void);  /* initialize GPIOs, setup SPI, DMA, ... */
+extern void hw_matrix_stop(void);  /* stop regular scanning (turn off LED matrix) */
+extern void hw_matrix_start(void); /* start regular scanning (turn on LED matrix) */
+extern void hw_matrix_mbi5029_mode(int special); /* change mbi5029 into/out of "special" mode */
+
 
 #endif
