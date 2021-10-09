aad24f847d931534375bbdff4a0568e6a977c722
Christian Vogel <vogelchr@vogel.cx>
2021-10-09 19:28:20 +0200

    README: minor addition, add define for panel type

--- README.txt
+++ README.txt
@@ -1,15 +1,14 @@
 Subway Sign Control using a BluePill stm32f103 Board
 
-
 Pins used:
 
         A0..A2: row select
-        A3: row driver output enable
+        A3: row driver output enable, low active
 
         A4: column driver latch enable
         A5: column driver clock
-        A6: column driver bit read back(?)
+        A6: column driver bit read back (not implemented)
         A7: column driver bit output
-        A8: column driver output enable
+        A8: column driver output enable (inverted)
 
         PC13: heartbeat LED
--- include/ledpanel_buffer.h
+++ include/ledpanel_buffer.h
@@ -4,9 +4,28 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+/*
+  select configuration, either one matrix module, or
+  the complete sign (using three matrix modules chained).
+*/
+
+#ifdef LEDPANEL_TYPE_SINGLE /* see platformio.ini */
+
 #define LEDPANEL_PIX_WIDTH 40
 #define LEDPANEL_PIX_HEIGHT 20
 #define LEDPANEL_SPI_BYTES 18 // 9 shifregisters in total
+
+#else
+#ifdef LEDPANEL_TYPE_TRIPLE
+
+#define LEDPANEL_PIX_WIDTH 120
+#define LEDPANEL_PIX_HEIGHT 20
+#define LEDPANEL_SPI_BYTES 54 // 27 shifregisters in total
+
+#else
+#error You need to define either LEDPANEL_TYPE_SINGLE or LEDPANEL_TYPE_TRIPLE!
+#endif
+#endif
 
 #define LEDPANEL_U32_PITCH ((LEDPANEL_PIX_WIDTH + 31) / 32)
 #define LEDPANEL_U8_PITCH ((LEDPANEL_PIX_WIDTH + 7) / 8)
