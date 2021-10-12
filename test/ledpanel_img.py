eab5ee12e3a0b9a188d8010d7d81e2ec49782cd5
Christian Vogel <vogelchr@vogel.cx>
2021-10-12 21:05:25 +0200

    ledpanel_img.py: quick hack for all-white

--- test/ledpanel_img.py
+++ test/ledpanel_img.py
@@ -25,7 +25,11 @@ if dev is None :
 
 dev.set_configuration()
 
-img = PIL.Image.open(args.pngfile)
+if args.pngfile.as_posix() == 'white':
+    img = PIL.Image.new('L', (args.width, args.height))
+    img.paste((255,), (0, 0, img.size[0], img.size[1]))
+else:
+    img = PIL.Image.open(args.pngfile)
 
 if img.size[1] != args.height :
     print('Image height must match panel height!')
@@ -37,9 +41,8 @@ if img.size[0] < args.width :
 # any control transfer will reset the write pointer ;-)
 dev.ctrl_transfer(0x40, 0)
 
-for dx in range(img.size[0] - args.width) :
+for dx in range(img.size[0] - args.width +1) :
     img_crop = img.crop((dx, 0, dx+args.width, args.height))
     output = ledpanel_tools.image_to_ledpanel_bytes(img_crop)
-    print(img_crop.size)
     dev.write(0x01, output)
     time.sleep(0.025)
